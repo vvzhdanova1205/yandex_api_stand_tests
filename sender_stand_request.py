031bfd69f515c9fd3fbed77a3451b3b872fa1d7b
Buka <vvzhdanova1205@yandex.ru>
2023-09-06 19:24:25 +0200

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,22 @@
+import configuration
+import requests
+import data
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+response = get_users_table()
+print(response.status_code)
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.text)
